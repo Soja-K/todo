a676c3800d3c5a4b5be68da323408c4ffcee5418
Soja-K <sojaayilur2001@gmail.com>
2022-11-24 14:19:28 +0530

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'newtodoproject';
+  userData:any=[
+    {name:"Soja",email:"soja200@gmail.com",address:"Kerala",position:"Angular devoloper"},
+    {name:"Sriya",email:"sriyaa101@gmail.com",address:"Hydrabad",position:"Python devoloper"},
+    {name:"Athira",email:"athira345@gmail.com",address:"Pune",position:"Odoo devoloper"},
+    {name:"Nandhini",email:"nandhu2897@gmail.com",address:"Mumbai",position:"Django devoloper"},
+    {name:"Drishya",email:"drishya645@gmail.com",address:"Madhya pradesh",position:"React devoloper"},
+    {name:"Biju",email:"kichu148@gmail.com",address:"Karnataka",position:"Data Analyst"},
+    {name:"Sudheesh",email:"sudhi904@gmail.com",address:"Tamilnadu",position:"Software engineer"},
+    {name:"Kavya",email:"kavi324@gmail.com",address:"Chennai",position:"Software Tester"},
+    {name:"Neelima",email:"neeli086@gmail.com",address:"Mysore",position:"Sql devoloper"},
+    {name:"Fasal",email:"gerrad370@gmail.com",address:"Goa",position:"System Analyst"},
+  ];
+  getCurrentUser(user:any):void{
+    console.log(user);
+
+}
+constructor(){
+  console.log(this.userData);
+}
 }
